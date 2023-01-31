582ae2f6607b304754c6da9f631203c6998d6bc4
YakoAba <101309040+YakoAba@users.noreply.github.com>
2023-01-30 21:28:45 -0300

    t

--- next.config.js
+++ next.config.js
@@ -8,3 +8,8 @@ module.exports = withPWA({
   // next.js config
   reactStrictMode: true,
 })
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+module.exports = withBundleAnalyzer({})
