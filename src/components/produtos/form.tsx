ce8718d680fe24d8781e9ba2ab3a2956a25ff7b8
YakoAba <101309040+YakoAba@users.noreply.github.com>
2023-02-22 16:40:58 -0300

    Update form.tsx

--- src/components/produtos/form.tsx
+++ src/components/produtos/form.tsx
@@ -1,6 +1,6 @@
-import { AddIcon } from "@chakra-ui/icons";
+
 import Grid from "./grid";
-import { Box, Button } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 import ModalCadProd from "./modal";
 import { useGlobalContext } from "../../contexts/GlobalContext";
 import ButtonAdicionar from "../Buttons/adicionar";
@@ -14,10 +14,10 @@ const FormularioProdutos = () => {
       sx={{ "::-webkit-scrollbar": { display: "none" } }}
       height="calc(100vh - 90px)"
       marginTop={20}
-      marginLeft="auto"
-      marginRight="auto"
-      paddingLeft="3"
-      paddingRight="3"
+      // marginLeft="auto"
+      // marginRight="auto"
+      // paddingLeft="3"
+      // paddingRight="3"
     >
       <ModalCadProd />
       <ButtonAdicionar mt={5} onClick={disclosureModalProdCad.onOpen} />
